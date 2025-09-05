b31ac0f24afbdd31e9d20475d51ed689f9a673e4
KawaiiTamago <168541837+KawaiiTamago@users.noreply.github.com>
2025-09-06 01:08:51 +0700

    Update mysqlbenchwork_query.sql

--- databases/mysql_query.sql
+++ databases/mysql_query.sql
@@ -203,8 +203,8 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `shopping-cart`;
-INSERT INTO `shopping-cart`.`user` (`email`, `name`, `mobile`, `address`, `pincode`, `password`) VALUES ('guest@gmail.com', 'Guest User', 9876543234, 'K.P Road, Gaya, Bihar - India', 879767, 'guest');
-INSERT INTO `shopping-cart`.`user` (`email`, `name`, `mobile`, `address`, `pincode`, `password`) VALUES ('admin@gmail.com', 'Admin User', 9876543459, 'ABC Colony, Newtown, West Bengal', 786890, 'admin');
+INSERT INTO `shopping-cart`.`user` (`email`, `name`, `mobile`, `address`, `pincode`, `password`) VALUES ('VuHaiDuong@gmail.com', 'Guest User', 0962933318, 'NguyenTrai, Hanoi', 10000, 'guest');
+INSERT INTO `shopping-cart`.`user` (`email`, `name`, `mobile`, `address`, `pincode`, `password`) VALUES ('CuongBui@gmail.com', 'Admin User', 0962783892, 'VoChiCong, HaNoi', 10000, 'admin');
 
 COMMIT;
 
